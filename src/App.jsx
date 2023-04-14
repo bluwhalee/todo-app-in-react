73f1adbcbf17a07117bfaa69b808a3f304b2b9dd
Ahmad Ali <ahmed.ali@mergestack.com>
2023-04-14 17:09:54 +0500

    master branch reverted

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import {Todo} from "./components/Todo.jsx";
+import {Form} from "./components/Form.jsx";
+function App() {
+
+
+  return (
+    <div>
+      <Todo />
+      <Form />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Todo.jsx
@@ -0,0 +1,9 @@
+import {useState} from "react";
+
+export function Todo() {
+
+
+    return (
+        <h1>Todo is do</h1>
+    )
+}
