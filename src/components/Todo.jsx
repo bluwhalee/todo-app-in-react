b53df8d5072f06ea2c0fed2aefafa54c898c9fb6
Ahmad Ali <ahmed.ali@mergestack.com>
2023-04-18 14:05:55 +0500

    used Font awesome and configured npm start

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,7 +4,10 @@ import {operationsReducer} from "../redux/todo/reducers/operations.js";
 import Icon, {icon} from "react-icons-kit"
 import {trash} from "react-icons-kit/feather/trash"
 import {edit2} from "react-icons-kit/feather/edit2"
-import {AddTodo} from "./AddTodo.jsx";
+import {AddTodo} from "./AddTodo.jsx"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+
 export function Todo() {
 
     const todos = useSelector((state) => state.operationsReducer);
@@ -20,8 +23,8 @@ export function Todo() {
                   <p style={todo.status===true?{textDecoration:"line-through"}:{textDecoration:"none"}}>{todo.task}</p>
               </div>
               <div>
-                  <Icon icon={trash}></Icon>
-                  <Icon icon={edit2}></Icon>
+                  <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
 
               </div>
           </div>
